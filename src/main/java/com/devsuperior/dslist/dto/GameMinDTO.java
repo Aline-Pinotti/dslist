e5adaae154c28a99f44c403499823dd8b0cc7c72
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-01-26 15:02:12 -0300

    Game DTO

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+    // Construtor que recebe a entidade (Game) e instancia o DTO
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+}
