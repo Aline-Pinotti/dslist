d669d26468c99540137c79d8015b9046b9a686eb
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-01 21:02:29 -0300

    Get Game By Id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -2,8 +2,11 @@ package com.devsuperior.dslist.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 import java.util.List;
@@ -17,6 +20,21 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameRepository.findAll().stream().map(x -> new GameMinDTO(x)).toList();
         return result;
+    }
+
+    // Boa prática colocar em todo método do service para que fique transacional.
+    // Para que garanta que
+    // a operação com o banco de dados vai acontecer obecenco aos princípios das
+    // transações (ACID - Atomicidade, Consistência, Isolamento e Durabilidade)
+    // Consistente, Isolado e Durável)
+    // read0nly = true: assegura que não vou fazer nenhuma operação de escrita - não
+    // bloqueia o banco de dados pra escrita
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        // Repository já tem um método chamado FindBy ID. Esse método retorna um
+        // Optional, então é necessário chamar o get() para pegar o objeto dentro
+        Game result = gameRepository.findById(id).get(); // TODO: tratamento para validar se o ID existe
+        return new GameDTO(result);
     }
 
 }
